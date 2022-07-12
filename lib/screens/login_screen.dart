b3b2d5052fd029a801ecede6f56106d8316cadba
Paul Joshua Premacio <paul.premacio.20@usjr.edu.ph>
2022-07-13 00:47:19 +0800

    added login api service

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,15 @@
+import 'package:fake_store/models/login_model.dart';
+import 'package:fake_store/screens/test.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 import '../services/api_service.dart';
 import 'home.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
+  ApiService get apiservice => GetIt.instance<ApiService>();
+  LoginRequestModel requestModel = LoginRequestModel();
 
   final TextEditingController nameCtrl = TextEditingController(
     text: 'mor_2314',
@@ -48,12 +53,12 @@ class LoginScreen extends StatelessWidget {
               height: 60,
               child: ElevatedButton(
                 onPressed: () async {
-                  final getToken = await login(
-                    nameCtrl.text,
-                    passwordCtrl.text,
-                  );
+                  requestModel.username = nameCtrl.text;
+                  requestModel.password = passwordCtrl.text;
+                  final getToken = await apiservice.login(requestModel);
 
-                  if (getToken != null && getToken['token'] != null) {
+                  if (getToken.token.isNotEmpty) {
+                    print("fuck");
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
                         content: Text('Successfully logged in'),
@@ -65,7 +70,7 @@ class LoginScreen extends StatelessWidget {
                       () => Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(
-                          builder: (_) => const HomeScreen(),
+                          builder: (_) => const test(),
                         ),
                       ),
                     );
